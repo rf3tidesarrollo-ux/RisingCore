9fa38c2eb8aaef73f0b0d6d685cc78d6912d4b09
rf3tidesarrollo-ux <rf3.ti.desarrollo@risingfarms.co>
2025-10-13 09:03:17 -0600

    Delete js directory

--- js/script.js
+++
@@ -1,4 +0,0 @@
-new DataTable('#basic-datatables');
-new DataTable('#basic-datatables1');
-new DataTable('#basic-datatables2');
-new DataTable('#basic-datatables3');
--- js/select.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.prueba').select2();
-});
-
-$("select").on("select2:open", function(event) {
-    $('input.select2-search__field').attr('placeholder', 'Buscar...');
-});
