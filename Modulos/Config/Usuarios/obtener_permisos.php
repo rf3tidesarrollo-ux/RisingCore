1d46165f505a93e8bf36f2ca3989644d76ab8344
rf3tidesarrollo-ux <rf3.ti.desarrollo@risingfarms.co>
2025-10-13 09:02:24 -0600

    Delete Modulos directory

--- Modulos/Config/Usuarios/obtener_permisos.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../../../Conexion/BD.php';
-$modulos = $Con->query("SELECT id_seccion, nombre_seccion FROM modulos")->fetch_all(MYSQLI_ASSOC);
-$permisos = $Con->query("SELECT id_permiso, nombre FROM permisos")->fetch_all(MYSQLI_ASSOC);
-
-echo json_encode(["modulos" => $modulos, "permisos" => $permisos]);
-
-?>
